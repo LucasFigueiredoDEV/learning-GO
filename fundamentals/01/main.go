bc50b39f6413f75eae665d773d5ed0977eee2eff
LucasFigueiredoDEV <lucassamuell.DEV@gmail.com>
2023-10-08 21:41:21 -0300

    Adicionando arquivos

---
+++ fundamentals/01/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	println(a)
+}
+
+// Variável "a" que foi declarada dentro do arquivo "a.go" sendo printada dentro do arquivo "main.go"
+// Tudo que estiver dentro da mesma pasta, será como se estivesse dentro de um mesmo arquivo
