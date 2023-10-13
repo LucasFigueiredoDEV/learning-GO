8bfef3c2bd862d157d85d85c309536a66725bd1c
LucasFigueiredoDEV <lucassamuell.DEV@gmail.com>
2023-10-13 11:10:12 -0300

    Adicionando novos arquivos de aulas concluidas e arquivo modificado

--- fundamentals/08/main.go
+++ fundamentals/08/main.go
@@ -2,13 +2,26 @@ package main
 
 import "fmt"
 
+var first_number = 8
+
+var second_number = 3
+
+var result = sum(first_number, second_number)
+
 func main() {
-	fmt.Println(sum(1, 2))
+	fmt.Println(sum_error(1, 2))
+
+	fmt.Printf("O valor da soma entre os números %d e %d é de %d", first_number, second_number, result)
 }
 
-func sum(a int, b int) (int, bool) {
+func sum(a int, b int) int {
+	return a + b
+}
+
+func sum_error(a int, b int) (int, bool) {
 	if a+b >= 50 {
 		return a + b, true
 	}
 	return a + b, false
-}
+} // Primeiro parêntese representa os parâmetros que serão passados e seus tipos
+// Segundo parêntese representa o tipo que será retornado
---
+++ fundamentals/09/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("O valor total da soma é de %d", sum(1, 3, 4, 10, 80, 15))
+}
+
+func sum(numeros ...int) int {
+	total := 0
+
+	for _, numero := range numeros {
+		fmt.Printf("Somando %d ao valor total\n", numero)
+		total += numero
+	}
+	return total
+}
